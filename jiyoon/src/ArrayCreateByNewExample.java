6d1e0ff300a0fb18a2c013803b9a40952db127ef
kwakjiyoon <yoonx.6227@gmail.com>
2026-04-15 10:08:25 +0900

    참조타입

--- jiyoon/src/ArrayCreateByNewExample.java
+++ jiyoon/src/ArrayCreateByNewExample.java
@@ -2,7 +2,7 @@ public class ArrayCreateByNewExample {
     public static void main(String[] args) {
         int[] arr1 = new int[3];
         for(int i=0; i<3; i++){
-            System.out.println("arr1["+ i +"]: " +arr1[i]);
+            System.out.println("arr1["+i+"]: " +arr1[i]);
         }
         arr1[0] = 10;
         arr1[1] = 20;
