f2331120376b66c82c9cab39032549b5b94ae790
kwakjiyoon <yoonx.6227@gmail.com>
2026-04-15 16:30:46 +0900

    5~7

---
+++ jiyoon/src/Calculator3.java
@@ -0,0 +1,17 @@
+public class Calculator3 { //메소드 오버로딩
+    
+    int plus(int x, int y){
+        return x+y;
+    }
+        double plus(double x, int y){
+            return x+y;
+        }
+
+        double plus(double x, int y){
+            return x+y;
+        }
+        double plus(double x, int y){
+            return x+y;
+        }
+    
+}
